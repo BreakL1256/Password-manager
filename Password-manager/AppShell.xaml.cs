4d606d856b8051aad38d077e7ff30c4007b9b386
Arnas Kičas <arnas.kicas@mif.stud.vu.lt>
2025-09-27 18:33:49 +0300

    Add project files.

---
+++ Password-manager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Password_manager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Password-manager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Password_manager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
