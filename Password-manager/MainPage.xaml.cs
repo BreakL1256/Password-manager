2d24f79780464a12bad7585258cd3a14f2d59a28
Arnas Kičas <arnas.kicas@mif.stud.vu.lt>
2025-09-28 20:11:10 +0300

    Padariau failo struktūrą ir pradėjau daryti UI

--- Password-manager/MainPage.xaml.cs
+++ Password-manager/MainPage.xaml.cs
@@ -1,24 +1,49 @@
-﻿namespace Password_manager
-{
-    public partial class MainPage : ContentPage
-    {
-        int count = 0;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
 
+namespace Password_manager
+{
+    public class PasswordItem
+    {
+        public PasswordItem(string Title, string Username, string Password)
+        {
+            this.Title = Title;
+            this.Username = Username;
+            this.Password = Password;
+        }
+        public string Title { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private PasswordItem? _selectedPassword;
+        public List<PasswordItem> PasswordList { get; set; } = new List<PasswordItem>();
         public MainPage()
         {
+            PasswordList.Add(new PasswordItem("gmail password","tom","abc"));
+            PasswordList.Add(new PasswordItem("netflix password","john", "cba"));
+
             InitializeComponent();
+
+            BindingContext = this;
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        public PasswordItem? SelectedPassword
         {
-            count++;
+            get => _selectedPassword;
+            set
+            {
+                _selectedPassword = value;
+                OnPropertyChanged();
+            }
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
